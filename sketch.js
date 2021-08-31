6ccc1172446d09a9ed70a4d250673e1dcfaeeab9
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-08-31 14:25:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  var num1=random(0,100);
+  var num2=random(0,100);
+  
+  result = num1 + num2;
+  console.log(result);
+  
+  
+}
+
+function draw() {
+  background(220);
+
+}
